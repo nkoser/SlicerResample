835df82d55171918d3341d0d36d70c2a805f2122
nkoser <58036930+nkoser@users.noreply.github.com>
2024-12-01 00:48:09 +0100

    Add files via upload

---
+++ ResamplingTransform/ResamplingTransform/ResamplingTransform.py
@@ -0,0 +1,349 @@
+import logging
+import os
+from typing import Annotated, Optional
+import SimpleITK as sitk
+import vtk
+
+import slicer
+from slicer.ScriptedLoadableModule import *
+from slicer.util import VTKObservationMixin
+from slicer.parameterNodeWrapper import (
+    parameterNodeWrapper,
+    WithinRange,
+)
+
+from slicer import vtkMRMLScalarVolumeNode, vtkMRMLTransformNode
+
+
+#
+# ResamplingTransform
+#
+
+class ResamplingTransform(ScriptedLoadableModule):
+    """Uses ScriptedLoadableModule base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self, parent):
+        ScriptedLoadableModule.__init__(self, parent)
+        self.parent.title = "ResamplingTransform"  # TODO: make this more human readable by adding spaces
+        self.parent.categories = [
+            "Examples"]  # TODO: set categories (folders where the module shows up in the module selector)
+        self.parent.dependencies = []  # TODO: add here list of module names that this module requires
+        self.parent.contributors = [
+            "John Doe (AnyWare Corp.)"]  # TODO: replace with "Firstname Lastname (Organization)"
+        # TODO: update with short description of the module and a link to online module documentation
+        self.parent.helpText = """
+This is an example of scripted loadable module bundled in an extension.
+See more information in <a href="https://github.com/organization/projectname#ResamplingTransform">module documentation</a>.
+"""
+        # TODO: replace with organization, grant and thanks
+        self.parent.acknowledgementText = """
+This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc., Andras Lasso, PerkLab,
+and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
+"""
+
+        # Additional initialization step after application startup is complete
+        slicer.app.connect("startupCompleted()", registerSampleData)
+
+
+#
+# Register sample data sets in Sample Data module
+#
+
+def registerSampleData():
+    """
+    Add data sets to Sample Data module.
+    """
+    # It is always recommended to provide sample data for users to make it easy to try the module,
+    # but if no sample data is available then this method (and associated startupCompeted signal connection) can be removed.
+
+    import SampleData
+    iconsPath = os.path.join(os.path.dirname(__file__), 'Resources/Icons')
+
+    # To ensure that the source code repository remains small (can be downloaded and installed quickly)
+    # it is recommended to store data sets that are larger than a few MB in a Github release.
+
+    # ResamplingTransform1
+    SampleData.SampleDataLogic.registerCustomSampleDataSource(
+        # Category and sample name displayed in Sample Data module
+        category='ResamplingTransform',
+        sampleName='ResamplingTransform1',
+        # Thumbnail should have size of approximately 260x280 pixels and stored in Resources/Icons folder.
+        # It can be created by Screen Capture module, "Capture all views" option enabled, "Number of images" set to "Single".
+        thumbnailFileName=os.path.join(iconsPath, 'ResamplingTransform1.png'),
+        # Download URL and target file name
+        uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
+        fileNames='ResamplingTransform1.nrrd',
+        # Checksum to ensure file integrity. Can be computed by this command:
+        #  import hashlib; print(hashlib.sha256(open(filename, "rb").read()).hexdigest())
+        checksums='SHA256:998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95',
+        # This node name will be used when the data set is loaded
+        nodeNames='ResamplingTransform1'
+    )
+
+    # ResamplingTransform2
+    SampleData.SampleDataLogic.registerCustomSampleDataSource(
+        # Category and sample name displayed in Sample Data module
+        category='ResamplingTransform',
+        sampleName='ResamplingTransform2',
+        thumbnailFileName=os.path.join(iconsPath, 'ResamplingTransform2.png'),
+        # Download URL and target file name
+        uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
+        fileNames='ResamplingTransform2.nrrd',
+        checksums='SHA256:1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97',
+        # This node name will be used when the data set is loaded
+        nodeNames='ResamplingTransform2'
+    )
+
+
+#
+# ResamplingTransformParameterNode
+#
+
+@parameterNodeWrapper
+class ResamplingTransformParameterNode:
+    """
+
+    """
+    inputVolume: vtkMRMLScalarVolumeNode
+    targetVolume: vtkMRMLScalarVolumeNode
+    outputVolume: vtkMRMLScalarVolumeNode
+    transform_: vtkMRMLTransformNode
+
+
+#
+# ResamplingTransformWidget
+#
+
+class ResamplingTransformWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
+    """Uses ScriptedLoadableModuleWidget base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self, parent=None) -> None:
+        """
+        Called when the user opens the module the first time and the widget is initialized.
+        """
+        ScriptedLoadableModuleWidget.__init__(self, parent)
+        VTKObservationMixin.__init__(self)  # needed for parameter node observation
+        self.logic = None
+        self._parameterNode = None
+        self._parameterNodeGuiTag = None
+
+    def setup(self) -> None:
+        """
+        Called when the user opens the module the first time and the widget is initialized.
+        """
+        ScriptedLoadableModuleWidget.setup(self)
+
+        # Load widget from .ui file (created by Qt Designer).
+        # Additional widgets can be instantiated manually and added to self.layout.
+        uiWidget = slicer.util.loadUI(self.resourcePath('UI/ResamplingTransform.ui'))
+        self.layout.addWidget(uiWidget)
+        self.ui = slicer.util.childWidgetVariables(uiWidget)
+
+        # Set scene in MRML widgets. Make sure that in Qt designer the top-level qMRMLWidget's
+        # "mrmlSceneChanged(vtkMRMLScene*)" signal in is connected to each MRML widget's.
+        # "setMRMLScene(vtkMRMLScene*)" slot.
+        uiWidget.setMRMLScene(slicer.mrmlScene)
+
+        # Create logic class. Logic implements all computations that should be possible to run
+        # in batch mode, without a graphical user interface.
+        self.logic = ResamplingTransformLogic()
+
+        # Connections
+
+        # These connections ensure that we update parameter node when scene is closed
+        self.addObserver(slicer.mrmlScene, slicer.mrmlScene.StartCloseEvent, self.onSceneStartClose)
+        self.addObserver(slicer.mrmlScene, slicer.mrmlScene.EndCloseEvent, self.onSceneEndClose)
+
+        # Buttons
+        self.ui.applyButton.connect('clicked(bool)', self.onApplyButton)
+
+        # Make sure parameter node is initialized (needed for module reload)
+        self.initializeParameterNode()
+
+    def cleanup(self) -> None:
+        """
+        Called when the application closes and the module widget is destroyed.
+        """
+        self.removeObservers()
+
+    def enter(self) -> None:
+        """
+        Called each time the user opens this module.
+        """
+        # Make sure parameter node exists and observed
+        self.initializeParameterNode()
+
+    def exit(self) -> None:
+        """
+        Called each time the user opens a different module.
+        """
+        # Do not react to parameter node changes (GUI will be updated when the user enters into the module)
+        if self._parameterNode:
+            self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
+            self._parameterNodeGuiTag = None
+            self.removeObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self._checkCanApply)
+
+    def onSceneStartClose(self, caller, event) -> None:
+        """
+        Called just before the scene is closed.
+        """
+        # Parameter node will be reset, do not use it anymore
+        self.setParameterNode(None)
+
+    def onSceneEndClose(self, caller, event) -> None:
+        """
+        Called just after the scene is closed.
+        """
+        # If this module is shown while the scene is closed then recreate a new parameter node immediately
+        if self.parent.isEntered:
+            self.initializeParameterNode()
+
+    def initializeParameterNode(self) -> None:
+        """
+        Ensure parameter node exists and observed.
+        """
+        # Parameter node stores all user choices in parameter values, node selections, etc.
+        # so that when the scene is saved and reloaded, these settings are restored.
+
+        self.setParameterNode(self.logic.getParameterNode())
+
+        # Select default input nodes if nothing is selected yet to save a few clicks for the user
+        if not self._parameterNode.inputVolume:
+            firstVolumeNode = slicer.mrmlScene.GetFirstNodeByClass("vtkMRMLScalarVolumeNode")
+            if firstVolumeNode:
+                self._parameterNode.inputVolume = firstVolumeNode
+
+    def setParameterNode(self, inputParameterNode: Optional[ResamplingTransformParameterNode]) -> None:
+        """
+        Set and observe parameter node.
+        Observation is needed because when the parameter node is changed then the GUI must be updated immediately.
+        """
+
+        if self._parameterNode:
+            self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
+            self.removeObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self._checkCanApply)
+        self._parameterNode = inputParameterNode
+        if self._parameterNode:
+            # Note: in the .ui file, a Qt dynamic property called "SlicerParameterName" is set on each
+            # ui element that needs connection.
+            self._parameterNodeGuiTag = self._parameterNode.connectGui(self.ui)
+            self.addObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self._checkCanApply)
+            self._checkCanApply()
+
+    def _checkCanApply(self, caller=None, event=None) -> None:
+        if self._parameterNode and self._parameterNode.inputVolume and \
+                self._parameterNode.outputVolume and self._parameterNode.transform_ and self._parameterNode.targetVolume:
+            self.ui.applyButton.toolTip = "Compute output volume"
+            self.ui.applyButton.enabled = True
+        else:
+            self.ui.applyButton.toolTip = "Select input and output volume nodes"
+            self.ui.applyButton.enabled = False
+
+    def onApplyButton(self) -> None:
+        """
+        Run processing when user clicks "Apply" button.
+        """
+        with slicer.util.tryWithErrorDisplay("Failed to compute results.", waitCursor=True):
+            self.logic.process(self.ui.inputSelector.currentNode(),
+                               self.ui.outputSelector.currentNode(),
+                               self.ui.transform.currentNode(),
+                               self.ui.targetSelector.currentNode())
+
+    #
+
+
+# ResamplingTransformLogic
+#
+
+class ResamplingTransformLogic(ScriptedLoadableModuleLogic):
+    """This class should implement all the actual
+    computation done by your module.  The interface
+    should be such that other python code can import
+    this class and make use of the functionality without
+    requiring an instance of the Widget.
+    Uses ScriptedLoadableModuleLogic base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self) -> None:
+        """
+        Called when the logic class is instantiated. Can be used for initializing member variables.
+        """
+        ScriptedLoadableModuleLogic.__init__(self)
+
+    def getParameterNode(self):
+        return ResamplingTransformParameterNode(super().getParameterNode())
+
+    def process(self,
+                inputVolume: vtkMRMLScalarVolumeNode,
+                outputVolume: vtkMRMLScalarVolumeNode,
+                transform_: vtkMRMLTransformNode,
+                targetVolume: vtkMRMLScalarVolumeNode,
+                showResult: bool = True) -> None:
+        """
+        Run the processing algorithm.
+        Can be used without GUI widget.
+        :param inputVolume: volume to be thresholded
+        :param outputVolume: thresholding result
+        :param showResult: show output volume in slice viewers
+        """
+
+        if not inputVolume or not outputVolume or not targetVolume:
+            raise ValueError("Input or output volume is invalid")
+
+        import time
+        startTime = time.time()
+        logging.info('Processing started')
+
+        inputImage = sitk.GetImageFromArray(slicer.util.arrayFromVolume(inputVolume))
+
+        # Get the transform matrix from the vtkMRMLTransformNode and convert it to a SimpleITK transform
+        #transformMatrix = transform_.GetMatrixTransformToParent()
+        #transformMatrix= slicer.util.arrayFromTransformMatrix(transform_)# 4x4 matrix
+        # Get the transform matrix from the vtkMRMLTransformNode
+        transformMatrix = transform_.GetMatrixTransformToParent()  # 4x4 matrix
+
+        # Convert vtkMatrix4x4 to a SimpleITK-compatible list
+        transformList = [transformMatrix.GetElement(i, j) for i in range(4) for j in range(4)]
+
+        # Create an AffineTransform with the matrix for 3D images
+        sitkTransform = sitk.AffineTransform(3)
+        sitkTransform.SetMatrix(transformList[:9])  # 3x3 matrix for rotation and scaling
+        sitkTransform.SetTranslation(transformList[9:12])  # translation (offset)
+
+        # Perform transformation and resampling using SimpleITK
+        targetSpacing = targetVolume.GetSpacing()
+        targetSize = targetVolume.GetImageData().GetDimensions()
+        targetDirection=targetVolume.GetImageData().GetDirection()
+
+        # Resample the input image with the transformation and target spacing
+        transformedResampledImage = sitk.Resample(inputImage,
+                                                  size=targetSize,  # Target size
+                                                  transform=sitkTransform,  # Apply the transformation
+                                                  interpolator=sitk.sitkLinear,  # Linear interpolation
+                                                  outputSpacing=targetSpacing,  # Target spacing
+                                                  outputOrigin=targetVolume.GetOrigin(),
+                                                  outputDirection=targetDirection,
+                                                  defaultValue=0)  # Background value
+
+        # Convert the transformed and resampled image back to a numpy array
+        resampledArray = sitk.GetArrayFromImage(transformedResampledImage)
+
+        # Store the result in the output volume
+        slicer.util.updateVolumeFromArray(outputVolume, resampledArray)
+
+        # Optionally, show the result in the slice viewers
+        if showResult:
+            slicer.app.processEvents()
+
+        # End timer
+        stopTime = time.time()
+        logging.info(f'Processing completed in {stopTime - startTime:.2f} seconds')
+
+#
+# ResamplingTransformTest
+#
